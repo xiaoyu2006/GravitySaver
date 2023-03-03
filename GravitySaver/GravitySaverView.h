2d738aa76cf740bfbb221b44a10bd9f3d9cea2d9
Yi Cao <caoyi06@qq.com>
2023-03-03 23:17:08 +0800

    Initial Commit

---
+++ GravitySaver/GravitySaverView.h
@@ -0,0 +1,12 @@
+//
+//  GravitySaverView.h
+//  GravitySaver
+//
+//  Created by Kerman on 2023/3/3.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface GravitySaverView : ScreenSaverView
+
+@end
